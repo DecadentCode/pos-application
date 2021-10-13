560dad3fca9fa76b177fad80c9fcc156915755a3
jeff robinson <jeff.h.robinson@icloud.com>
2021-10-13 17:25:20 -0400

    added javascript, removed html

--- script.js
+++ script.js
@@ -1,64 +1,50 @@
 "use strict";
 
-// const ticketArray = [
-//   {
-//     name: "General Admission",
-//     category: "tickets",
-//     description: "Basic admissions",
-//     price: 40,
-//   },
-//   {
-//     name: "Medium Admission",
-//     category: "tickets",
-//     description: "Medium Admissions",
-//     price: 100,
-//   },
-//   {
-//     name: "Grande Admission",
-//     category: "tickets",
-//     description: "Grande Admissions",
-//     price: 10000,
-//   },
-// ];
+const clothingList = document.querySelector(".clothing-list");
+const collectablesList = document.querySelector(".collectables-list");
 
-ticketArray.forEach();
-
-const goodsArrary = [
+const goodsArray = [
   {
     name: "Hoodie",
     category: "clothes",
     description: "hoodie",
     price: 300,
+    picture: "assets/hoodie.png",
   },
   {
     name: "Hat",
     category: "clothes",
     description: "hat",
     price: 0.5,
+    picture: "assets/hat.png",
   },
   {
     name: "bandana",
     category: "clothes",
     description: "bandana",
     price: 5,
+    picture: "assets/bandana.png",
   },
   {
     name: "Womens shirt",
     category: "clothes",
     description: "womens shirt",
     price: 5,
+    picture: "assets/rick-croptop.png",
   },
   {
     name: "Mens shirt",
     category: "clothes",
     description: "mens shirt",
     price: 99,
+    picture: "assets/dudeshirt.png",
   },
   {
     name: "Dog beanies",
     category: "clothes",
     description: "dog beanie",
     price: 10,
+    picture: "assets/dogbeanie.png",
   },
 ];
 
@@ -68,23 +54,43 @@ const collectablesArray = [
     category: "collectables",
     description: "keychain",
     price: 3,
+    picture: "assets/keychain.png",
   },
   {
     name: "Mugs",
     category: "collectables",
     description: "mugs",
     price: 10,
+    picture: "assets/mug.png",
   },
   {
     name: "Pins",
     category: "collectables",
     description: "pins",
     price: 8,
+    picture: "assets/pins.png",
   },
   {
     name: "Magnets",
     category: "collectables",
     description: "magnets",
     price: 10,
+    picture: "assets/magnets.png",
   },
 ];
+
+const goodsFunction = (array, destination) => {
+  array.forEach((item) => {
+    const listItem = document.createElement("li");
+    const title = document.createElement("h3");
+    const image = document.createElement("img");
+    listItem.classList.add("not-tickets");
+    title.textContent = item.name;
+    image.setAttribute("src", item.picture);
+    listItem.append(title, image);
+    destination.append(listItem);
+  });
+};
+
+goodsFunction(goodsArray, clothingList);
+goodsFunction(collectablesArray, collectablesList);
