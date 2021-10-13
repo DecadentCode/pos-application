4f0f8a000388e2b6e76c759077b31ab5b90017f5
nick <nwagner566@yahoo.com>
2021-10-13 15:17:52 -0400

    Added arrays,iamges

--- script.js
+++ script.js
@@ -1,1 +1,90 @@
 "use strict";
+
+const ticketArray = [
+  {
+    name: "General Admission",
+    category: "tickets",
+    description: "Basic admissions",
+    price: 40,
+  },
+  {
+    name: "Medium Admission",
+    category: "tickets",
+    description: "Medium Admissions",
+    price: 100,
+  },
+  {
+    name: "Grande Admission",
+    category: "tickets",
+    description: "Grande Admissions",
+    price: 10000,
+  },
+];
+
+ticketArray.forEach();
+
+const goodsArrary = [
+  {
+    name: "Hoodie",
+    category: "clothes",
+    description: "hoodie",
+    price: 300,
+  },
+  {
+    name: "Hat",
+    category: "clothes",
+    description: "hat",
+    price: 0.5,
+  },
+  {
+    name: "bandana",
+    category: "clothes",
+    description: "bandana",
+    price: 5,
+  },
+  {
+    name: "Womens shirt",
+    category: "clothes",
+    description: "womens shirt",
+    price: 5,
+  },
+  {
+    name: "Mens shirt",
+    category: "clothes",
+    description: "mens shirt",
+    price: 99,
+  },
+  {
+    name: "Dog beanies",
+    category: "clothes",
+    description: "dog beanie",
+    price: 10,
+  },
+];
+
+const collectablesArray = [
+  {
+    name: "Keychains",
+    category: "collectables",
+    description: "keychain",
+    price: 3,
+  },
+  {
+    name: "Mugs",
+    category: "collectables",
+    description: "mugs",
+    price: 10,
+  },
+  {
+    name: "Pins",
+    category: "collectables",
+    description: "pins",
+    price: 8,
+  },
+  {
+    name: "Magnets",
+    category: "collectables",
+    description: "magnets",
+    price: 10,
+  },
+];
